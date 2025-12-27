56bdc3a236b71813e43970cd7a37968e698b8662
AppalanaiduBangaru <naidubangaru81@gmail.com>
2025-12-27 19:29:14 +0530

    Add Render domain to ALLOWED_HOSTS

--- student_project/settings.py
+++ student_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$s6t1#xq)1@thif(8vzvfvg=_41!zb^nd!cq*w@fjssnd%&jiz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "django-sms-2.onrender.com"]
 
 
 # Application definition
